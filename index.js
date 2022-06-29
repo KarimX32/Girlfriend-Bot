ba130256f7cf9547de88e06316bbd3c866bf77b5
UltraX <67840146+KarimX32@users.noreply.github.com>
2022-06-29 21:30:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ client.on("speech", async (message) => {
     let voiceConnection = await getVoiceConnection(message.guild.id);
 
     a = await reply(message.author.id, message.content)
+  
+    // to see all supported languages head to "https://api.ultrax-yt.com/v1/details/supported-tts-languages" 
+    // and add at the end of the URL << &language='en' >> ofc change en with your prefered language.
     resource = createAudioResource(`https://api.ultrax-yt.com/v1/tts?key=${process.env.X}&query=${a}`)
 
     voiceConnection.subscribe(player)
